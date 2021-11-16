821e1e29855a0ddef656c91aa3f551a734fbba05
unknown <khachikpurt85@gmail.com>
2021-11-16 16:13:14 +0400

    terst

---
+++ js/Skript.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+  text += " * " + i + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+</script>
+
+</body>
